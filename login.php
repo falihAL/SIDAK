b0888502367683947dbc15d8b9b2fd94f667984f
falihAL <falihadelio23@gmail.com>
2024-07-06 15:15:34 +0700

    mengubah login

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>login | SIDAK</title>
+	<title>login | Data Penduduk</title>
 	<link rel="icon" href="dist/img/izin.png">
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
