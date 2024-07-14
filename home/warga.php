828f73193b60aa5b8787f3c5f418a502321bdd14
falihAL <falihadelio23@gmail.com>
2024-07-14 13:51:14 +0700

    menambahkan halaman warga

---
+++ home/warga.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logo Kabupaten Tangerang</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+        .logo-container {
+            margin-top: 50px;
+            text-align: center;
+        }
+        .logo-container img {
+            max-width: 100%;
+            height: auto;
+            margin: 20px;
+            border: 5px solid #ddd;
+            border-radius: 10px;
+            transition: transform 0.2s;
+        }
+        .logo-container img:hover {
+            transform: scale(1.05);
+        }
+        .header-title {
+            margin-bottom: 30px;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <div class="container logo-container">
+        <h3 class="header-title">DOKUMENTASI</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="./dist/img/balaraja2.jpeg" alt="Logo Balaraja" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <img src="./dist/img/logokabtang2.png" alt="Logo Kabupaten Tangerang" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <img src="./dist/img/acarahut.jpg" alt="HUT Balaraja" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <img src="./dist/img/rthbalaraja.jpg" alt="RTH" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <img src="./dist/img/nyoblos.jpeg" alt="Nyoblos" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
