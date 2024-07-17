56b168f941b4a548933d929d9655b697fc38b78d
gilangabian123 <rtama4866@gmail.com>
2024-07-17 22:45:04 +0700

    mengubah href pada suket lahir (kaur)

--- index.php
+++ index.php
@@ -371,7 +371,7 @@
 									</a>
 								</li>
 								<li class="nav-item">
-									<a href="#" class="nav-link">
+									<a href="?page=suket-lahir" class="nav-link">
 										<i class="nav-icon far fa-circle text-warning"></i>
 										<p>Su-Ket Kelahiran</p>
 									</a>
