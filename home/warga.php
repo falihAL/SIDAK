89a4799a51ccf887145273b64ded78a693ee25d6
falihAL <falihadelio23@gmail.com>
2024-07-17 23:38:46 +0700

    menambahkan deskripsi foto

--- home/warga.php
+++ home/warga.php
@@ -13,41 +13,66 @@
             margin-top: 50px;
             text-align: center;
         }
-        .logo-container img {
-            max-width: 100%;
-            height: auto;
+        .logo-container figure {
             margin: 20px;
             border: 5px solid #ddd;
             border-radius: 10px;
             transition: transform 0.2s;
+            overflow: hidden;
         }
-        .logo-container img:hover {
+        .logo-container img {
+            max-width: 100%;
+            height: auto;
+        }
+        .logo-container figure:hover {
             transform: scale(1.05);
         }
         .header-title {
             margin-bottom: 30px;
             color: black;
         }
+        .figure-caption {
+            text-align: center;
+            padding: 10px;
+            background-color: #fff;
+            border-top: 1px solid #ddd;
+        }
     </style>
 </head>
 <body>
     <div class="container logo-container">
-        <h3 class="header-title">DOKUMENTASI</h3>
+        <h3 class="header-title">ABOUT BALARAJA</h3>
         <div class="row">
             <div class="col-md-6">
-                <img src="./dist/img/balaraja2.jpeg" alt="Logo Balaraja" class="img-fluid">
+                <figure>
+                    <img src="./dist/img/balaraja2.jpeg" alt="Logo Balaraja" class="img-fluid">
+                    <figcaption class="figure-caption">Nama Balaraja berasal dari dua kata yaitu Bala atau Bale dan Raja. Bale berarti balai: tempat persinggahan dan pertemuan.
+                    Raja adalah pemimpin tertinggi, Jadi Balaraja adalah tempat yang disediakan untuk peristirahatan sementara bagi para pemimpin untuk mengatur strategi perang atau persiapan untuk menuju medan perang.</figcaption>
+                </figure>
             </div>
             <div class="col-md-6">
-                <img src="./dist/img/logokabtang2.png" alt="Logo Kabupaten Tangerang" class="img-fluid">
+                <figure>
+                    <img src="./dist/img/logokabtang2.png" alt="Logo Kabupaten Tangerang" class="img-fluid">
+                    <figcaption class="figure-caption"> Sejarah Kabupaten Tangerang diriwayatkan bermula saat Kesultanan Banten terdesak oleh Agresi Militer Belanda pada pertengahan abad ke-16, diutuslah tiga maulana yang berpangkat Tumenggung untuk mem­buat perkampungan pertahanan di wilayah yang berbatasan dengan Batavia.</figcaption>
+                </figure>
             </div>
             <div class="col-md-6">
-                <img src="./dist/img/acarahut.jpg" alt="HUT Balaraja" class="img-fluid">
+                <figure>
+                    <img src="./dist/img/acarahut.jpg" alt="HUT Balaraja" class="img-fluid">
+                    <figcaption class="figure-caption">Kegiatan HUT kecamatan Balaraja yang ke- 143 berlangsung di Ruang Terbuka Hijau (RTH) Desa Talaga Sari, kecamatan Balaraja, kabupaten Tangerang, yang di hadiri oleh sekda kabupaten Tangerang dan pelepasan jalan santai dan turut serta dalam kegiatan jalan santai serta hadir seluruh kepala Desa yang ada di wilayah kecamatan Balaraja, serta tokoh masyarakat, tokoh pemuda, tokoh agama dan seluruh masyarakat kecamatan Balaraja yang ikut memeriahkan acara HUT kecamatan Balaraja yang ke 143, Rabu 12 Juni 2024.</figcaption>
+                </figure>
             </div>
             <div class="col-md-6">
-                <img src="./dist/img/rthbalaraja.jpg" alt="RTH" class="img-fluid">
+                <figure>
+                    <img src="./dist/img/rthbalaraja.jpg" alt="RTH" class="img-fluid">
+                    <figcaption class="figure-caption">Peresmian Ruang Terbuka Hijau Balaraja</figcaption>
+                </figure>
             </div>
             <div class="col-md-6">
-                <img src="./dist/img/nyoblos.jpeg" alt="Nyoblos" class="img-fluid">
+                <figure>
+                    <img src="./dist/img/nyoblos.jpeg" alt="Nyoblos" class="img-fluid">
+                    <figcaption class="figure-caption">Sosialisasi Pencoblosan di MAN 2 TANGERANG</figcaption>
+                </figure>
             </div>
         </div>
     </div>
